2f94ab3b220f9c9cac55b09d59af89ac27fc8985
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-03-10 10:24:48 -0600

    Add files via upload

---
+++ CircleArea.cpp
@@ -0,0 +1,19 @@
+/*
+Circle Area
+Pawelski
+3/14/2021
+Please follow the instructions on the activity sheet for what needs
+to be completed.
+*/
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    // declare a constant called pi with the value 3.14159
+    // declare a variable called radius with the value 4
+    double area = pi * pow(radius, 2);
+    std::cout << "Area of the circle: " << area;
+    return 0;
+}
